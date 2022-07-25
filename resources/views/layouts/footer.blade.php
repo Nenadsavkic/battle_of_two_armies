7a68a393029a860f81a1f859fd5195ed2a49f1a4
Nenad Savkic <savkicn@gmail.com>
2022-07-25 16:26:20 +0200

    finished

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
           <div class="row">
               <div class=" col-md-6 offset-md-3 footer-content ">
-                <p class="text-center p-5 mt-1">Copyrights <small>&#169;</small> Nenad Savkic 2022.</p>
+                <p class="text-center p-5 mt-1">Battle of two armies</p>
              </div>
           </div>
     </div>
